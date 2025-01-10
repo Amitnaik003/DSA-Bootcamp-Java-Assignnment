cfec1f848ddd1c5e604753c284f068e425894267
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-01-10 03:05:31 +0000

    Add files via upload

---
+++ Flow Of program/multiplicationtable.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class multiplicationtable {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int m = sc.nextInt();
+        for(int i=0;i<=10;i++){
+            System.out.println(m+" X " +i+ " = " +m*i);
+        }
+}
+}
