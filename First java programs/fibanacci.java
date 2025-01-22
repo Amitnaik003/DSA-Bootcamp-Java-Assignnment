4c007b911cf195d8996033beb0269878924b679c
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-01-22 13:06:45 +0000

    Add files via upload

---
+++ First java programs/fibanacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class fibanacci {
+    public static void main(String[] args) {
+        int n,a=0,b=1,c;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of terms");
+        n=sc.nextInt();
+        for(int i=0;i<=n;i++){
+            System.out.println(a);
+            c=a+b;
+            a=b;
+            b=c;
+        }
+}
+}
