5fdf267f5e3461f8acf6d62d7f26d337162384d9
Amit Naik <154658171+Amitnaik003@users.noreply.github.com>
2025-02-14 04:51:05 +0000

    Add files via upload

---
+++ conditionloops/basicjavaprograms/additionof2.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class additionof2 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number 1:");
+        int a=sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum of 2 numbers is equal to:"+sum);
+    }
+}
